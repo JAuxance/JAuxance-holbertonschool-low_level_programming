697eb35a57d8be1c4fc603ae620ba9d4137cce29
JAuxance <auxance.jourdan@proton.me>
2025-10-21 10:46:03 +0200

    featr: prog for print one phrase

---
+++ hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* programe to prints a phrase */
+int main(void)
+{
+    char *mytext="\"Programming is like building a multilingual puzzle";
+    puts (mytext);
+    return 0;
+}
+    
