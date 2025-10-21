1faf9cd93d2c992e8c10934a05e2dd95e91fd12e
JAuxance <auxance.jourdan@proton.me>
2025-10-21 13:40:44 +0200

    featr: prog to print a specific phrase

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print a specific phrase to the standard output
+ *
+ * Return: Always 0 (Sucesse)
+ */
+int main(void)
+{
+char mytext[] = "with proper grammar, but the outcome is a piece of art,";
+
+printf("String: %s\n" ,mytext);
+
+return(0);
+}
