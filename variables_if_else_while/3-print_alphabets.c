1cb02baa6a5d69a8fe2a6b489f45618f90d92390
JAuxance <auxance.jourdan@proton.me>
2025-10-22 22:58:50 +0200

    fix: resize line 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the alphabet in lowercase and uppercase, followed by a new line.
+ * main - prints the alphabet in lowercase and uppercase.
  *
  * Return: Always 0 (Success)
  */
