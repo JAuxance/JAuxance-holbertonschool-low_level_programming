36ffa88aa083e75b7181e324a19d7a20a60989f9
JAuxance <auxance.jourdan@proton.me>
2025-10-22 22:53:33 +0200

    featr: print alpha in lower and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stido.h>
+/**
+ * 
+ */
+int main(void)
+{
+char *alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i = 0;
+
+while (alpha[i] != '\0')
+{
+putchar(alpha[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
