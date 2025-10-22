ab54d9c6b393562acdd418d206966aa0f5afd2fe
JAuxance <auxance.jourdan@proton.me>
2025-10-22 14:46:22 +0200

    featr: prog for print last digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: programe qui dit quand le nombre aléatoire de la f
+ *
+ * Return: Alawys 0 (Sucesse)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last_digit = n % 10;
+{
+printf("Last digit of %d is %d\n", last_digit, n);
+}
+if (last_digit > 5)
+{
+printf("and is greater than 5");
+}
+if (last_digit == 0)
+{
+printf ("and is 0");
+}
+if (last_digit < 6 && last_digit < 0)
+{
+printf ("and is less than 6 and not 0")
+}
+return (0);
+}
