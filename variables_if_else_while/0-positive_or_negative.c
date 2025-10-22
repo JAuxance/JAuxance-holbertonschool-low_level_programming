428d7276f0195ad562c9e1065acd21a0cee9a21f
JAuxance <auxance.jourdan@proton.me>
2025-10-22 11:28:15 +0200

    featr:program that says whether the randomly generated number is positive or negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: programe qui dit quand le nombre aléatoire de la variable et positif ou négatif
+ *
+ * Return: Alawys 0 (Sucesse)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if(n > 0)
+{
+printf("is positive\n");
+}
+if(n == 0);
+{
+printf("is zero\n");
+}
+if(n < 0);
+{
+printf("is negative\n");
+}
+
+	return (0);
+}
