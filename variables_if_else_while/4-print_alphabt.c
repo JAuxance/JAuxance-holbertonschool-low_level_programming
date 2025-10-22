cdebd1be0f0fa3daf81fb521fab09156dd9734eb
JAuxance <auxance.jourdan@proton.me>
2025-10-22 23:13:33 +0200

    featr: print alpha except e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *alpha = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+}
+while (alpha[i] != '\0')
+{
+if (alpha[i] != 'e' && alpha[i] != 'q')
+putchar(alpha[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
