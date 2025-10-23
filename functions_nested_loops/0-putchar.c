28cc7965f6579a766f4e0c13af1eaeef7c3d9c43
JAuxance <auxance.jourdan@proton.me>
2025-10-23 16:53:22 +0200

    featr: print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints _putchar, followed by a new line.
+ *
+ * Return: Alawys 0 (Sucesse)
+ */
+int main(void)
+{
+  char *ch= "_putchar";
+ printf("%s\n", ch);
+}
