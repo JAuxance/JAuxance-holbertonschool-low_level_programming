475e313d3eca3d039c1f92ff4daafb5ffed8bdd7
JAuxance <auxance.jourdan@proton.me>
2025-10-23 11:36:17 +0200

    featr: print alpha in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *alpha = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+
+while (alpha[i] != '\0')
+i++;
+
+while (i >= 0)
+{
+	putchar(alpha[i]);
+	i--;
+}
+putchar('\n');
+return (0);
+}
