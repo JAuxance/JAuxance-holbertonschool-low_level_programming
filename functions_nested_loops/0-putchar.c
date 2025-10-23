ea5d8ece5ab16698fe9fd10a4a6cad564e3d2a6f
JAuxance <auxance.jourdan@proton.me>
2025-10-23 17:12:40 +0200

    featr: print _putchar with wirht

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Description: prints _putchar, followed by a new line.
  *
  * Return: Alawys 0 (Sucesse)
  */
+int _putchar(char c);
 int main(void)
 {
-char *ch = "_putchar";
-{
-printf("%s\n", ch);
-return (0);
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
