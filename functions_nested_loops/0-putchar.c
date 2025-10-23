efeca0a4842f09ed0f605e3efa59f116ed2b6d81
JAuxance <auxance.jourdan@proton.me>
2025-10-23 17:37:31 +0200

    test

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,31 +1,20 @@
-#include <unistd.h>
-/**
- * _putchar - écrit un caractère sur la sortie standard
- * @c: caractère à afficher
- *
- * Return: 1 si succès, -1 si erreur et errno est défini
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include "main.h"
+
 /**
  * main - Entry point
  *
- *
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
