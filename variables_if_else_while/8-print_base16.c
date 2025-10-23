65af390185afb8e326df3853a2e09c65add944a5
JAuxance <auxance.jourdan@proton.me>
2025-10-23 13:51:33 +0200

    featr: the numbers of base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:  using printf to print numbers from 0 to 9
+ *
+ * Return: Alawys 0 (Sucesse)
+ */
+int main(void)
+{
+char *decimal;
+char *digit;
+
+digit = "0123456789";
+decimal = "abcdef";
+{
+	printf("%s%s\n", digit, decimal);
+}
+	return (0);
+}
