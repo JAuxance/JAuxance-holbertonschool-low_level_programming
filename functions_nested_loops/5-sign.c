e6c9cb08ebd2ddcd1713a3d022fa0d49c646fd0d
JAuxance <auxance.jourdan@proton.me>
2025-10-24 15:24:28 +0200

    fix: add desc

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,6 +1,14 @@
 #include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: number to be checked
+ *
+ * Return: 1 if n is greater than zero,
+ * 0 if n is zero,
+ * -1 if n is less than zero
+ */
 int print_sign(int n)
-{ 
+{
 if (n > 0)
 {
 _putchar('+');
