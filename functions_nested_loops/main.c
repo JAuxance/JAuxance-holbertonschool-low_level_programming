4b18399f12d5c58fa06508b0f9ac478ea38fa3d2
JAuxance <auxance.jourdan@proton.me>
2025-10-24 15:04:27 +0200

    featr: checks for alphabetic character.

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -10,6 +10,8 @@ print_alphabet_x10();
 _putchar('\n');
 _putchar(_islower('a') + '0');
 _putchar(_islower('A') + '0');
+_putchar(_isalpha('a') + '0');
+_putchar(_isalpha('A') + '0');
 _putchar('\n');
 return (0);
 }
