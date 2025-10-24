d241b66bfecd6f8d7f560337b39381d95f746482
JAuxance <auxance.jourdan@proton.me>
2025-10-24 12:20:21 +0200

    fix:  deleted ; from the for loop

--- functions_nested_loops/print_alphabet_x10.c
+++ functions_nested_loops/print_alphabet_x10.c
@@ -1,5 +1,10 @@
 #include "main.h"
-void print_alphabet_x10(void);
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
 {
     for (int i = 0; i < 10; i++)
     {
