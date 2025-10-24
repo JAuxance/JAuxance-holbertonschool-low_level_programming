4176ecaddfcb26dd09b1c9f165d7888611f76e37
JAuxance <auxance.jourdan@proton.me>
2025-10-24 12:38:23 +0200

    featr: print 10time alpha follow by a nawline

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet in lowercase
- *
- * Description: This function prints the lowercase alphabet 10 times,
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
  *
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    for (i = 0; i < 10; i++)
-    {
-        print_alphabet();
-    }
+int i;
+for (i = 0; i < 10; i++)
+print_alphabet();
 }
