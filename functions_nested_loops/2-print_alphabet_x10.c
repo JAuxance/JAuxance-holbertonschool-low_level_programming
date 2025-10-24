f86ce0193b75838f80b4129fa05e2c8976916f31
JAuxance <auxance.jourdan@proton.me>
2025-10-24 12:41:24 +0200

    fix: call print alpha in 2-print

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,6 +4,18 @@
  *
  * Return: void
  */
+void print_alphabet(void)
+{
+char *alpha = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+
+while (alpha[i] != '\0')
+{
+_putchar(alpha[i]);
+i++;
+}
+_putchar('\n');
+}
 void print_alphabet_x10(void)
 {
 int i;
