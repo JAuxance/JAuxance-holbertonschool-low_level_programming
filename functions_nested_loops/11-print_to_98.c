2297c15b62e4ba2164abe1707a517ea497e5f962
JAuxance <auxance.jourdan@proton.me>
2025-10-25 17:39:29 +0200

    featr: new function that prints all natural numbers from n to 98. tx printf

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the number to start printing from
+ * Return: void
+ */
+void print_to_98(int n);
+{
+if (n <= 98)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+printf("98\n");
+}
+else
+{
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+printf("98\n");
+}
+}
