b7cfce88d4a6af9d618bcab764bed0fd25cca301
JAuxance <auxance.jourdan@proton.me>
2025-10-27 16:45:41 +0100

    fix: ''

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,11 @@
 void print_most_numbers(void)
 {
 int num;
+
 for (num = 0; num <= 9; num++)
 {
-if (num != '2' && num != '4')
-
-{
+if (num != 2 && num != 4)
 _putchar(num + '0');
- num++;
-}
-
 }
 _putchar('\n');
 }
