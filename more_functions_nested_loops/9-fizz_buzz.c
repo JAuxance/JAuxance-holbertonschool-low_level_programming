3ba1ab92a38a94013726cb25f16440948e7e371f
JAuxance <auxance.jourdan@proton.me>
2025-10-28 15:08:33 +0100

    fix: betty style

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,20 +8,21 @@
  *
  * Return: Always 0.
  */
-int main(void) 
+int main(void)
 {
-int n, num;
-for (n = 1; n < 100; n++)
-{
-if (n % 3 == 0 && n % 5 == 0)
-printf("FizzBuzz");
-else if (n % 3 == 0)
-printf("Fizz");
-else if (n % 5 == 0)
-printf("Buzz");
-else
-printf("%d", n);
-if (n < 100)
-printf(" ");
-}
+	int n, num;
+
+	for (n = 1; n < 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		if (n < 100)
+			printf(" ");
+	}
 }
