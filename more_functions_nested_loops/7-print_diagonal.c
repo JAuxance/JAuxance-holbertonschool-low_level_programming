84b9df4e24552924f0cded1dcd14c155d11449d3
JAuxance <auxance.jourdan@proton.me>
2025-10-28 10:15:58 +0100

    featr: new function that draws a diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+int i;
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 0; i < n; i++)
+{
+_putchar('\\');
+_putchar('\n');
+}
+_putchar('\n');
+}
