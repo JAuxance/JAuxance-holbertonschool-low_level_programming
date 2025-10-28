999a6c12059364cbbe9239323a2c86f0ace5beef
JAuxance <auxance.jourdan@proton.me>
2025-10-28 10:28:29 +0100

    fix: loop 'for'

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,7 +6,7 @@
  */
 void print_diagonal(int n)
 {
-int i;
+int i, j;
 if (n <= 0)
 {
 _putchar('\n');
@@ -14,10 +14,8 @@ return;
 }
 for (i = 0; i < n; i++)
 {
-for (n = 0; i < n; i++)
-{
+for (j = 0; j < i; j++)
 _putchar(' ');
-}
 _putchar('\\');
 _putchar('\n');
 }
