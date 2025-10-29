63cfd756dfe18385bf7e8d3f3498ac7797d503a2
JAuxance <auxance.jourdan@proton.me>
2025-10-29 16:33:58 +0100

    feat: add rev_string function to reverse a string and update main.h

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include 'main.h'
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char temp;
+
+	while (s[i] != '\0')
+		i++;
+
+	i = i - 1;
+
+	while (j < i)
+	{
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+		j++;
+		i--;
+	}
+}
