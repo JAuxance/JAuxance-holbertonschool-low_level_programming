7c95aa6155451c627697c8742b2c092dfae3f1b8
JAuxance <auxance.jourdan@proton.me>
2025-10-29 17:13:27 +0100

    fix: refactor puts_half function to calculate string length manually and simplify logic

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,24 +1,26 @@
 #include "main.h"
-#include <string.h>
 /**
  * puts_half - prints half of a string, followed by a new line
  * @str: string to print
  */
 void puts_half(char *str)
 {
-	int len = strlen(str);
+
+	int len = 0;
 	int start;
 
-	if (len % 2 == 0)
-		start = len / 2;
-	else
-		start = (len - 1) / 2 + 1;
-	while (str[start] != '\0')
-	{
-		_putchar(str[start]);
-		start++;
-	}
-	{
-		_putchar('\n');
-	}
-	}
+while (str[len] != '\0')
+	len++;
+
+start = len / 2;
+
+while (str[start] != '\0')
+{
+	_putchar(str[start]);
+	start++;
+}
+
+{
+	_putchar("\n");
+}
+}
