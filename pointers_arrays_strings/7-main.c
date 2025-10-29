edefeb648eabd4eb6686952c377070b33458403b
JAuxance <auxance.jourdan@proton.me>
2025-10-29 16:55:35 +0100

    feat: add puts_half function to print half of a string and update main.h

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * main - check the code
@@ -11,6 +10,6 @@ int main(void)
 	char *str;
 
 	str = "0123456789";
-	puts2(str);
+	puts_half(str);
 	return (0);
 }
---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
+ */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int start;
+
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len - 1) / 2 + 1;
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	{
+		_putchar('\n');
+	}
+	}
