aa4e11b6488a29ade6f24960d7fc30369dbf319b
JAuxance <auxance.jourdan@proton.me>
2025-10-30 20:13:18 +0100

    feat: implement _strcat function to concatenate two strings and update main.h

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	j = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+
+	}
+
+	while (src[j] != '\0')
+
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
