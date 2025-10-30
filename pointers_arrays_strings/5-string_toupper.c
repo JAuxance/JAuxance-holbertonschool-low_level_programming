03c5fff7cb9bc1126c26964d2af8ffd841879e47
JAuxance <auxance.jourdan@proton.me>
2025-10-30 23:01:21 +0100

    feat: implement string_toupper function to convert all lowercase letters of a string to uppercase and update main.h

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: pointer to the string to be modified
+ * Return: pointer to the modified string
+ */
+char *string_toupper(char *s)
+
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'Z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
