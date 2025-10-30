86100148ff9dd1701a090aa4e26932e7180ec326
JAuxance <auxance.jourdan@proton.me>
2025-10-30 20:15:35 +0100

    fix: update documentation for _strcat function to clarify parameters and return value

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- *
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to destination string
  */
 char *_strcat(char *dest, char *src)
 {
