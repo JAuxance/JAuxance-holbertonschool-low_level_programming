1ba6341e1fb722d88bc9e1bba0505703801decbc
JAuxance <auxance.jourdan@proton.me>
2025-11-03 14:31:15 +0100

    feat: implement _strstr function to locate a substring in a string

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search
+ * @needle: substring to locate
+ * Return: pointer to beginning of located substring, or NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+
+			if (haystack[i] == needle[j])
+		{
+			while (haystack[i + j] == needle[j] && needle[j] != '\0')
+			{
+				j++;
+			}
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
+			}
+		}
+
+		i++;
+	}
+	return (NULL);
+}
