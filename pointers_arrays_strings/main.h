e696d92651618e9625aec91bc0ba0c3655959506
JAuxance <auxance.jourdan@proton.me>
2025-11-03 15:22:23 +0100

    feat: implement print_diagsums function to calculate and print the sums of the diagonals of a square matrix

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,4 +25,5 @@ char *_strpbrk(char *s, char *accept);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 #endif
