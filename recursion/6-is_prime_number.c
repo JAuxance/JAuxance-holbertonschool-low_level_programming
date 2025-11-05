dc730665679d1c235b16613cd4abd9b966c61e0e
JAuxance <auxance.jourdan@proton.me>
2025-11-05 18:05:40 +0100

    feat: add is_prime_number function to determine if a number is prime

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * is_prime_helper - helper function to determine if a number is prime
+ * @n: number to check
+ * @i: iterator
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_helper(int n, int i)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	if (i * i > n)
+	{
+		return (1);
+	}
+	return (is_prime_helper(n, i + 1));
+}
+/**
+ * is_prime_number - determines if a number is prime
+ * @n: number to check
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_helper(n, 2));
+}
