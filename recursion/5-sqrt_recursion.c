94e038df2062af9b5b03a2ef4282ef1bd82de488
JAuxance <auxance.jourdan@proton.me>
2025-11-05 17:54:13 +0100

    fix: remove unnecessary blank line in _sqrt_helper function

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,7 +7,6 @@
  * square root
  */
 int _sqrt_helper(int n, int i)
-
 {
 	if (i * i > n)
 	return (-1);
@@ -16,8 +15,13 @@ int _sqrt_helper(int n, int i)
 	return (i);
 	return (_sqrt_helper(n, i + 1));
 }
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find the square root of
+ * Return: the natural square root of n or -1 if n does not have a natural
+ * square root
+ */
 int _sqrt_recursion(int n)
 {
 	return (_sqrt_helper(n, 0));
 }
-
