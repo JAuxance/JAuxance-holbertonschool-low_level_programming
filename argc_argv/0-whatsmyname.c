f10278377e0f4f47085c77c9de59b5dcb0b32a1c
JAuxance <auxance.jourdan@proton.me>
2025-11-06 17:15:57 +0100

    fix: replace puts with _putchar to print program name character by character

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -11,7 +11,15 @@
 int main(int argc, char *argv[])
 {
 	{
-		puts(argv[0]);
+		(void)argc;
+		int i = 0;
+
+		while (argv[0][i] != '\0')
+		{
+			_putchar(argv[0][i]);
+			i++;
+		}
+		_putchar('\n');
 	}
-	return (0);
+		return (0);
 }
