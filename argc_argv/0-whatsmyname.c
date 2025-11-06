7bdbcaf7a5f496098603c005142b9906ac2f3a38
JAuxance <auxance.jourdan@proton.me>
2025-11-06 16:48:49 +0100

    feat: implement _putchar function and add main.h header file

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+		printf("%s\n", argv[0]);
+}
