223457bf0a40720d9822c0e7c489e607dfc72634
JAuxance <auxance.jourdan@proton.me>
2025-11-07 14:40:30 +0100

    fix: update main function documentation for clarity

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - prints the number of arguments passed into it
+ * main - prints the number of arguments passed to the program
  * @argc: number of arguments
  * @argv: array of arguments
+ * Return: 0
  */
-
 	int main(int argc, char *argv[])
 
 {
