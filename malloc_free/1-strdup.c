8932bb2d6d3e6747b5e58fc88d64ce204233e93e
JAuxance <auxance.jourdan@proton.me>
2025-11-12 13:30:13 +0100

    feat: add _strdup function to duplicate strings and update main header

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+	#include "main.h"
+	#include <stdio.h>
+	#include <stdlib.h>
+	/**
+
+	 * _strdup - duplicates a string
+	 * @str: string to duplicate
+	 * Return: pointer to the duplicated string
+	 */
+	char *_strdup(char *str)
+
+	{
+		char *dest;
+		int i;
+		int len = 0;
+
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+
+		while (str[len] != '\0')
+		{
+			len++;
+		}
+
+		dest = malloc(sizeof(char) * (len + 1));
+
+		if (dest == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			dest[i] = str[i];
+		}
+		dest[i] = '\0';
+		return (dest);
+	}
