af6b9bf58bc92f5273528e40cfd25ec9d0733f88
JAuxance <auxance.jourdan@proton.me>
2025-11-13 15:16:29 +0100

    feat: implement string_nconcat function to concatenate two strings with specified length

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to concatenate from s2
+ * Return: pointer to the newly allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len = 0, len2 = 0, take = 0, j = 0, i = 0;
+	char *new = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len] != '\0')
+		len++;
+	while (s2[len2] != '\0')
+		len2++;
+	if (n >= len2)
+		take = len2;
+	else
+		take = n;
+	new = malloc(sizeof(char) * (len + take + 1));
+	if (new == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while (j < take)
+	{
+		new[i + j] = s2[j];
+		j++;
+	}
+	new[i + j] = '\0';
+	return (new);
+}
