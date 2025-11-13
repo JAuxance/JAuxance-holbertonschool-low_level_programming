16ab14201c3b191e139b0a8ee1af370408af7386
JAuxance <auxance.jourdan@proton.me>
2025-11-13 14:03:18 +0100

    fix: correct documentation for malloc_checked function

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocatez
+ * Return: pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
