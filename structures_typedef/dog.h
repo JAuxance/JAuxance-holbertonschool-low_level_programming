78f57d3129a4125bf95ab235f6b6b3e1f965d611
JAuxance <auxance.jourdan@proton.me>
2025-11-14 12:28:19 +0100

    fix: reorder init_dog function declaration in dog.h and remove main.h include

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-void init_dog(struct dog *d, char *name, float age, char *owner);
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
  * struct dog - structure for dog information
  * @name: name of the dog
@@ -17,5 +15,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
