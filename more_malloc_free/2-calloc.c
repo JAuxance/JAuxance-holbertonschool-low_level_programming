85edded040a9875d06c5e0bebeababbbb506097f
JAuxance <auxance.jourdan@proton.me>
2025-11-14 10:50:58 +0100

    feat: implement _calloc and _putchar functions for memory allocation and character output

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int total = nmemb * size;
+	char *ptr;
+	int i = 0;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(total);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < total)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
