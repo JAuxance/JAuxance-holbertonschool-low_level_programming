16f9f26c937809359ed221605eebcdb8f0aa3b47
JAuxance <auxance.jourdan@proton.me>
2025-11-14 14:12:20 +0100

    fix: reorder includes and improve struct dog definition

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
  * struct dog - structure for dog information
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+/* Prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif /* DOG_H */
