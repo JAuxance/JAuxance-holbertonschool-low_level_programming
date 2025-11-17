6b8d88422f19fa7200ee159a503dcbd47eca7e96
JAuxance <auxance.jourdan@proton.me>
2025-11-17 15:01:11 +0100

    fix: correct header guard name in function_pointers.h and add VSCode settings for file association

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,4 @@
-#ifndef fonction_pointers_h
-#define fonction_pointers_h
-#include <stdio.h>
+#ifndef function_pointers_h
+#define function_pointers_h
 void print_name(char *name, void (*f)(char *));
 #endif
