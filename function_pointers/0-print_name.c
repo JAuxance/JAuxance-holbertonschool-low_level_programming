da4d850c7ae273495e1c201dd19e8a5cfc7b0314
JAuxance <auxance.jourdan@proton.me>
2025-11-17 14:23:05 +0100

    test

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_name - prints a name using a function pointer
+ * @name: the name to print
+ * @f: function pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		return;
+	}
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
