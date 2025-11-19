4585c47874bba30de85899bf05c58e8d47807c33
JAuxance <auxance.jourdan@proton.me>
2025-11-19 11:18:38 +0100

    feat: implement calculator functions and main logic for operation selection

---
+++ function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, or exit with error code
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
