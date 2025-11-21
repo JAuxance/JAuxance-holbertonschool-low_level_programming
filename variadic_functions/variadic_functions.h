caa3a8fca0713bbd1314144576151d6257157ec8
JAuxance <auxance.jourdan@proton.me>
2025-11-21 13:49:27 +0100

    feat: add print_strings function to print strings with a separator

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,9 @@
 #define variadic_fonctions_h
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif /* variadic_fonctions_h */
